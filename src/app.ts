e33836379da8684b5b0ffebfe5e3513f9128298e
Shafaet-j <lbshaft01@gmail.com>
2023-11-24 01:19:22 +0600

    getall user done

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1/users", UserRoutes);
+app.use("/", UserRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,5 +3,6 @@ import { UserController } from "./user.controller";
 const router = express.Router();
 
 router.post("/create-user", UserController.createUser);
+router.get("/api/users", UserController.getAllUser);
 
 export const UserRoutes = router;
